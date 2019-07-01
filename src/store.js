ad07e48a83bb6d145a1eecfa0e8c67e500feeb0d
klimash <a.klimash@icloud.com>
2019-07-01 20:58:30 +0300

    :sparkles: Store: posts

--- src/store.js
+++ src/store.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import apiPosts from '@/api/posts'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    posts: []
   },
   mutations: {
-
+    addPosts (state, posts) {
+      state.posts = state.posts.concat(posts)
+    }
   },
   actions: {
-
+    async addPosts ({ commit }, count) {
+      const posts = await apiPosts.get(count)
+      commit('addPosts', posts)
+    }
   }
 })
