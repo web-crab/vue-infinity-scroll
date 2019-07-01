37ccb73047e2f5b399522b671f65ba8525cf754b
klimash <a.klimash@icloud.com>
2019-07-01 21:00:01 +0300

    :sparkles: Blog page: add to router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -9,15 +8,14 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      meta: { linkText: 'Home' },
+      component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/blog',
+      name: 'blog',
+      meta: { linkText: 'Blog' },
+      component: () => import(/* webpackChunkName: "blog" */ '@/views/Blog.vue')
     }
   ]
 })
